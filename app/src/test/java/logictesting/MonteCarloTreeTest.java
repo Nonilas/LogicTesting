bac3fc6bcd021b088e40d03b7f950c8b695a83e4
Nonilas <achilleasleiv@gmail.com>
2023-12-12 17:13:39 +0100

    gg

--- app/src/test/java/logictesting/MonteCarloTreeTest.java
+++ app/src/test/java/logictesting/MonteCarloTreeTest.java
@@ -1,38 +1,68 @@
 package logictesting;
- 
 
 import logictesting.Logic.Bot.MonteCarloNode;
 import logictesting.Logic.Bot.MonteCarloTree;
 import logictesting.Logic.Card;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
 public class MonteCarloTreeTest {
 
-    @Test
-    public void testMonteCarloNode() {
-        ArrayList<Card> deck = new ArrayList<>();
+    private MonteCarloNode monteCarloNode;
+    private ArrayList<Card> deck;
+
+    @BeforeEach
+    public void setUp() {
+        deck = new ArrayList<>();
         deck.add(new Card(Card.Type.NUMBER, Card.Color.RED, 5));
         deck.add(new Card(Card.Type.SKIP, Card.Color.BLUE, -1));
+        monteCarloNode = new MonteCarloNode(deck, new Card(Card.Type.NUMBER, Card.Color.RED, 5));
+    }
 
-        MonteCarloNode monteCarloNode = new MonteCarloNode(deck, new Card(Card.Type.NUMBER, Card.Color.RED, 5));
-
+    @Test
+    public void testMonteCarloNodeInitialization() {
         assertNotNull(monteCarloNode);
         assertNotNull(monteCarloNode.getChildNodes());
         assertEquals(1, monteCarloNode.getVisitCount());
         assertEquals(0, monteCarloNode.getTotalScore());
-         
         assertEquals(0, monteCarloNode.getChildNodes().size());
-
-        // Add more assertions as needed
     }
 
+    @Test
+    public void testNodeExpansion() {
+        monteCarloNode.expandNode();
+        assertTrue(monteCarloNode.getChildNodes().size() > 0);
+    }
+
+    @Test
+    public void testRollOut() {
+        monteCarloNode.RollOut();
+        assertTrue(monteCarloNode.getTotalScore() >= 0);
+    }
+
+    @Test
+    public void testSelectBestChildNode() {
+        MonteCarloTree monteCarloTree = new MonteCarloTree();
+        monteCarloTree.rootNode = monteCarloNode;
+        monteCarloNode.expandNode();
+
+        Card bestCard = monteCarloTree.search(monteCarloNode);
+        assertNotNull(bestCard);
+    }
+
+    @Test
+    public void testTotalScoreCalculation() {
+        monteCarloNode.expandNode();
+        monteCarloNode.calcTotalScore();
+        assertTrue(monteCarloNode.getTotalScore() >= 0);
+    }
     
     }
 
-    // Add more test methods for MonteCarloNode and MonteCarloTree as needed
+    
 
 
